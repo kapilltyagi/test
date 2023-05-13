6fe56e02b89b05f3e725b5ad995fd639b7ce3844
kapil <kapill.tyagi@gmail.com>
2023-05-13 12:31:31 +0530

    local added

--- src/main/java/Kapil.java
+++ src/main/java/Kapil.java
@@ -2,6 +2,13 @@ public class Kapil{
   public static void main(String args[]){
     int i=10;
       System.out.println("Hello from local");
+      int j=20;
+      System.out.println(j);
+      local();
+  }
+
+  static void local(){
+    System.out.println("Locaalllll");
   }
 
 }
