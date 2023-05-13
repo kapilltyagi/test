88cc9e42cb7e16784ac38b9958fd874fa656edbd
kapilltyagi <kapill.tyagi@gmail.com>
2023-05-13 12:31:07 +0530

    Update Kapil.java

--- src/main/java/Kapil.java
+++ src/main/java/Kapil.java
@@ -2,6 +2,13 @@ public class Kapil{
   public static void main(String args[]){
     int i=10;
       System.out.println("Hello from local");
+    int j=30;
+    System.out.println(j);
+    remote();
   }
 
+  static void remote(){
+     System.out.println("Remoteeeee");
+
+  }
 }
